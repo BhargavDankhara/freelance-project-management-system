629e743b88792849d425db78a85064bbb55f3762
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-20 00:41:27 +0530

    review, order, message routes and controllers added....

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ import { connectDB } from "./config/db.js";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import gigRoute from "./routes/gig.route.js";
+import reviewRoute from "./routes/review.route.js";
+import orderRoute from "./routes/order.route.js";
+import messageRoute from "./routes/message.route.js";
+import conversationRoute from "./routes/conversation.route.js";
 
 const app = express();
 
@@ -20,11 +24,17 @@ const PORT = ENV_VARS.PORT;
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something went wrong!" });
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
 });
 
 app.listen(PORT, () => {
